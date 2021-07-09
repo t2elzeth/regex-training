7225d1f4f53dd582e6035959a7caa131b94c93db
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-09 23:52:37 +0600

    Udpate

--- regex_version.py
+++ regex_version.py
@@ -10,9 +10,13 @@ URL = 'https://github.com/yyx990803/vue-svelte-size-analysis'
 
 
 def extract_data(url: str):
-    mypattern = r'^https://github.com/(?P<username>.*)/(?P<repo_name>.*)(?:/(.*)+)?$'
+    mypattern = r'https://github.com/(?P<username>.*)/(?P<repo_name>.*)(?:/(.*)+)?'
     match = re.match(mypattern, url)
     return match.groupdict() if match is not None else {}
 
 
 print(extract_data(URL))
+print(extract_data(URL + '/'))
+print(extract_data(URL + '/pulls'))
+print(extract_data(URL + '/pulls/'))
+print(extract_data(URL + '/pulls/ldflsdlflsd'))
