5cb971bfa07c11a8386a3b1ffff652f383408274
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-10 00:37:41 +0600

    Integrate REGEX

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-from urllib.parse import urlparse
+import re
 from string import Template
-import requests
 
-API_URL_TEMPLATE = "https://api.github.com/repos/{username}/{repo_name}/pulls"
+import requests
 
 TEMPLATE = Template('https://api.github.com/repos/$username/$repo_name/pulls')
 URL = "https://github.com/yyx990803/vue-svelte-size-analysis"
 
 # Generate API URL
-url_path = urlparse(URL).path
-splitted_url = url_path.split("/")
-username = splitted_url[1]
-repo_name = splitted_url[2]
+mypattern = r'https://github.com/(?P<username>[^/]*)/(?P<repo_name>[^/]*)'
+match = re.match(mypattern, URL)
+template_data = match.groupdict() if match is not None else {}
 
-api_url = API_URL_TEMPLATE.format(username=username, repo_name=repo_name)
-print("Built following API URL: ", api_url)
-print('Built following API URL2: ', TEMPLATE.substitute(username=username, repo_name=repo_name))
+api_url = TEMPLATE.substitute(**template_data)
+print('Built following API URL: ', api_url)
 
 # Make request to GithubAPI
-# response = requests.get(api_url).json()
-# print("This is a list of pull requests: ", response)
+response = requests.get(api_url).json()
+print("This is a list of pull requests: ", response)
